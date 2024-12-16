870ef64fd13126d1202e9bcb2958017de015c5f9
Christopher Stöckl <christopher@cstoeckl.de>
2024-12-16 14:37:34 +0100

    Add GuessInput component with form validation and styling

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState("");
+
+  function handleChange(event) {
+    setGuess(event.target.value.toUpperCase());
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log({ guess });
+    setGuess("");
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        required
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        value={guess}
+        onChange={handleChange}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
